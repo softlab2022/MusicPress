3fc4a2dbee6cfed9b1098d1afae9167d5762baa2
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-05-23 14:41:02 +0600

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function(){
+   
+   $('.side-btn').click(function(){
+     $('.sidebar').toggleClass('openmenu');
+   });
+   
+   $('.side-btn').click(function(){
+     $('.sidebar-icon').toggleClass('fa-bars fa-close');
+   });
+   
+
+   
+   
+   })
